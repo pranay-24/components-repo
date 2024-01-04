39b9732550bab2b0a62afe2106dd0a2c04fc052e
Pranay Mangal <mangal.pranay@gmail.com>
2024-01-04 04:33:19 -0500

    fontawesome added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Menu , Disclosure} from "@headlessui/react";
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import  {faBars}  from '@fortawesome/free-solid-svg-icons';
+
 export default function Navbar() {
   const userRole = "user";
 
